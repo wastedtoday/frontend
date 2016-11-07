4014235d02ce3481e2336bca78cc6a6fedf1851e
Lukas Schönbächler <schoenbaechler.lukas@gmail.com>
2016-11-07 10:40:45 +0100

    Pull data from backend

--- src/Categories.js
+++ src/Categories.js
@@ -4,15 +4,26 @@ import {Tabs, Tab} from 'material-ui/Tabs';
 
 
 export default class Categories extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {categories: []};
+
+        fetch(`http://wasted.today/server.api/category/`)
+            .then(result=>result.json())
+            .then(categories=>this.setState({categories}));
+    }
+
+
     render() {
         return (
             <Tabs>
-                <Tab label="Technology">
-                    <Items/>
-                </Tab>
-                <Tab label="Lifestyle">
-                    <Items />
-                </Tab>
-            </Tabs>        );
+                {this.state.categories.map(category=>
+                    <Tab label={category.name} key={category.id}>
+                        <Items items={category.items}/>
+                    </Tab>
+                )}
+            </Tabs>
+        );
     }
 }
--- src/Items.js
+++ src/Items.js
@@ -4,17 +4,24 @@ import Avatar from 'material-ui/Avatar';
 import {Link} from 'react-router'
 
 export default class Items extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <List>
-                <Link to={'/item/1'}>
-                    <ListItem
-                        leftAvatar={<Avatar src="https://bower.io/img/bower-logo.png" />}
-                        rightIcon={<span style={{overflow:'hidden'}}>20 hours</span>}
-                        primaryText="Bower"
-                        secondaryText="https://bower.io"
-                    />
-                </Link>
+                {this.props.items.map(item=>
+                    <Link to={'/item/' + item.id} key={item.id}>
+                        <ListItem
+                            leftAvatar={<Avatar src={item.imageUrl} />}
+                            rightIcon={<span style={{overflow:'hidden'}}>20 hours</span>}
+                            primaryText={item.name}
+                            secondaryText={item.referenceUrl}
+                        />
+                    </Link>
+                )}
             </List>
         );
     }
