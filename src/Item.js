d183c98541c4ff9de6cb3f0f258e5ef3221e74d9
Lukas Schönbächler <schoenbaechler.lukas@gmail.com>
2016-11-07 09:45:40 +0100

    Item dialog added

--- src/Item.js
+++ src/Item.js
@@ -1,7 +1,49 @@
 import React from 'react';
+import Dialog from 'material-ui/Dialog';
+import RaisedButton from 'material-ui/RaisedButton';
+import FlatButton from 'material-ui/FlatButton';
 
-const Item = (props) => (
-    <h1>haha</h1>
-);
+export default class Item extends React.Component {
+    state = {
+        open: false,
+    };
 
-export default Item;
+    handleOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
+    render() {
+        const actions = [
+            <FlatButton
+                label="Cancel"
+                primary={true}
+                onTouchTap={this.handleClose}
+            />,
+            <FlatButton
+                label="Submit"
+                primary={true}
+                keyboardFocused={true}
+                onTouchTap={this.handleClose}
+            />,
+        ];
+
+        return (
+            <div>
+                <RaisedButton label="Dialog" onTouchTap={this.handleOpen} />
+                <Dialog
+                    title="Dialog With Actions"
+                    actions={actions}
+                    modal={false}
+                    open={this.state.open}
+                    onRequestClose={this.handleClose}
+                >
+                    The actions in this window were passed in as an array of React objects.
+                </Dialog>
+            </div>
+        );
+    }
+}
